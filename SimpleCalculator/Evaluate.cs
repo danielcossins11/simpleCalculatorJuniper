a9f60b468e811ba605ac8a2f12535153b7bda211
danielcossins11 <daniel.cossins@yahoo.com>
2015-11-18 15:16:58 -0600

    made Evaluate Class

---
+++ SimpleCalculator/Evaluate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public class Evaluate
+    {
+        public Evaluate(int num1, char op, int num2)
+        {
+            
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtact(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+
+        public int Modulus(int a, int b)
+        {
+            return a % b;
+        }
+    }
+}
