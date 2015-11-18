29f02722c7ff71114ac715338155b4eebe894168
danielcossins11 <daniel.cossins@yahoo.com>
2015-11-18 13:36:29 -0600

    first commit

---
+++ SimpleCalculator/ProcessInput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleCalculatorFromScratch
+{
+    public class ProcessInput
+    {
+        public string Input { get; set; }
+        public ProcessInput(string input)
+        {
+            Input = input;
+        }
+    }
+}
