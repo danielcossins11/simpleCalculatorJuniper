f642b0d14aae0eb90c97051c2d0dbaf5e500dd05
danielcossins11 <daniel.cossins@yahoo.com>
2015-11-18 13:39:10 -0600

    first test passing

--- SimpleCalculatorTests/UnitTest1.cs
+++ SimpleCalculatorTests/UnitTest1.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleCalculator;
 
-namespace SimpleCalculatorTests
+namespace UnitTestProject1
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ProcessInputEnsureParameterIsCorrect()
         {
+            string expected = "hello";
+            ProcessInput PI = new ProcessInput(expected);
+
+            Assert.AreEqual(expected, PI.Input);
         }
     }
 }
